14e2f679f4381b7b375b5cb7b145bfe93ab0bfbe
rachd <rachel.m.dorn@gmail.com>
2022-07-16 09:41:45 -0700

    update to react 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { hot } from "react-hot-loader";
 import "./App.css";
 
 const App = () => {
@@ -10,4 +9,4 @@ const App = () => {
   );
 };
 
-export default hot(module)(App);
+export default App;
